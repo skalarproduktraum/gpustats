b9b9f2b06c467d1992d44ae7d9a2bbf7b12898a3
Ulrik Günther <ulrik@42degreesoffreedom.com>
2015-04-16 15:23:38 +0200

    Added menu to menu bar icon, shows occupancy and item to quit the application.

--- gpustats/AppDelegate.m
+++ gpustats/AppDelegate.m
@@ -13,13 +13,13 @@
 
 @property (weak) IBOutlet NSWindow *window;
 @property (strong, nonatomic) NSStatusItem *statusItem;
-@property (assign, nonatomic) NSMenu *statMenu;
 @property (assign, nonatomic) BOOL darkModeOn;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    NSMenu *statMenu = [[NSMenu alloc] init];
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     _statusItem.image = [NSImage imageNamed:@"menubar-icon"];
     
@@ -31,8 +31,13 @@
     unsigned long long free = gs_getFreeGlobalMem();
     
     _statusItem.toolTip = [NSString stringWithFormat:@"Free global/Total global:\n%.0fM/%.0fM, %.0f%% free", free/1048576.0f, total/1048576.0f, (float)free/(float)total*100.0f];
-
-
+    
+    [statMenu addItemWithTitle: @"GPUstats" action:nil keyEquivalent:@""];
+    [statMenu addItemWithTitle: [NSString stringWithFormat:@"Free/Total: %.0fM/%.0fM", free/1048576.0f, total/1048576.0f] action:nil keyEquivalent:@""];
+    [statMenu addItem:[NSMenuItem separatorItem]];
+    [statMenu addItemWithTitle:@"Quit" action:@selector(quit:) keyEquivalent:@""];
+    
+    _statusItem.menu = statMenu;
     
     [NSTimer scheduledTimerWithTimeInterval:2.0
                                      target:self
@@ -41,12 +46,18 @@
                                     repeats:YES];
 }
 
+- (void)quit:(id)sender {
+    [[NSApplication sharedApplication] terminate:self];
+}
+
 - (void)reenquireGPUStatus:(id)sender {
     gs_setDevice(0);
     unsigned long long total = gs_getTotalGlobalMem();
     unsigned long long free = gs_getFreeGlobalMem();
     
     _statusItem.toolTip = [NSString stringWithFormat:@"Free global/Total global:\n%.0fM/%.0fM, %.0f%% free", free/1048576.0f, total/1048576.0f, (float)free/(float)total*100.0f];
+    
+    [[_statusItem.menu itemAtIndex:1] setTitle:[NSString stringWithFormat:@"Free/Total: %.0fM/%.0fM", free/1048576.0f, total/1048576.0f]];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
@@ -54,12 +65,7 @@
 }
 
 - (void)itemClicked:(id)sender {
-    NSEvent *event = [NSApp currentEvent];
-    
-    if([event modifierFlags] & NSControlKeyMask) {
-        [[NSApplication sharedApplication] terminate:self];
-        return;
-    }
+
 }
 
 - (void)refreshDarkMode {
