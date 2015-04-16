184a1f6b861709e007d3356edcd367a2733da775
Ulrik Günther <ulrik@42degreesoffreedom.com>
2015-04-16 13:24:46 +0200

    Initial Commit

---
+++ gpustats/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gpustats
+//
+//  Created by Ulrik Guenther on 16/04/15.
+//  Copyright (c) 2015 ulrik.is. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ gpustats/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  gpustats
+//
+//  Created by Ulrik Guenther on 16/04/15.
+//  Copyright (c) 2015 ulrik.is. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
