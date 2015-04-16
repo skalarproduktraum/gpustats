5463d2bd89251ab0d315cbe1ffb7a88719cf80b4
Ulrik Günther <ulrik@42degreesoffreedom.com>
2015-04-16 17:40:24 +0200

    Tracking of app starts/quits
    * gpustats keeps track of started and quit apps for correlating
      memory occupancy with certain programs in a plot lateron

--- gpustats/AppDelegate.m
+++ gpustats/AppDelegate.m
@@ -14,12 +14,15 @@
 @property (weak) IBOutlet NSWindow *window;
 @property (strong, nonatomic) NSStatusItem *statusItem;
 @property (assign, nonatomic) BOOL darkModeOn;
+@property (strong, nonatomic) NSMutableArray *appTimepoints;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     NSMenu *statMenu = [[NSMenu alloc] init];
+    _appTimepoints = [[NSMutableArray alloc] init];
+    
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     _statusItem.image = [NSImage imageNamed:@"menubar-icon"];
     
@@ -44,6 +47,13 @@
                                    selector:@selector(reenquireGPUStatus:)
                                    userInfo:nil
                                     repeats:YES];
+    
+    NSMutableArray *runningApps = [[NSMutableArray alloc] init];
+    for(NSRunningApplication *app in [[NSWorkspace sharedWorkspace] runningApplications]) {
+        [runningApps addObject:[NSString stringWithString:[app localizedName]]];
+    }
+    
+    [_appTimepoints addObject:runningApps];
 }
 
 - (void)quit:(id)sender {
@@ -58,6 +68,34 @@
     _statusItem.toolTip = [NSString stringWithFormat:@"Free global/Total global:\n%.0fM/%.0fM, %.0f%% free", free/1048576.0f, total/1048576.0f, (float)free/(float)total*100.0f];
     
     [[_statusItem.menu itemAtIndex:1] setTitle:[NSString stringWithFormat:@"Free/Total: %.0fM/%.0fM", free/1048576.0f, total/1048576.0f]];
+    
+    NSMutableArray *runningApps = [[NSMutableArray alloc] init];
+    for(NSRunningApplication *app in [[NSWorkspace sharedWorkspace] runningApplications]) {
+        [runningApps addObject:[NSString stringWithString:[app localizedName]]];
+    }
+    
+    if([_appTimepoints count] >= 20) {
+        [_appTimepoints removeObjectAtIndex:0];
+    }
+    
+    // check whether any apps have been started or quit
+    
+    NSSet *original = [NSSet setWithArray:[_appTimepoints lastObject]];
+    NSSet *new = [NSSet setWithArray:runningApps];
+    
+    NSMutableSet *starts = [NSMutableSet setWithCapacity: [runningApps count]];
+    NSMutableSet *quits = [NSMutableSet setWithCapacity:[[_appTimepoints lastObject] count]];
+    
+    [starts addObjectsFromArray:runningApps];
+    [quits addObjectsFromArray:[_appTimepoints lastObject]];
+    
+    [starts minusSet:original];
+    [quits minusSet:new];
+
+    [_appTimepoints addObject:runningApps];
+    
+    NSLog(@"Starts: %@", starts);
+    NSLog(@"Quits: %@", quits);
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
